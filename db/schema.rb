89c8bac88c5b7ccc9661975ed3c7ea3013319711
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2019-05-31 00:22:30 -0300

    db postgresql in production

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2019_05_30_201211) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,8 +37,8 @@ ActiveRecord::Schema.define(version: 2019_05_30_201211) do
   end
 
   create_table "client_discounts", force: :cascade do |t|
-    t.integer "client_id"
-    t.integer "discount_id"
+    t.bigint "client_id"
+    t.bigint "discount_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["client_id"], name: "index_client_discounts_on_client_id"
@@ -55,7 +58,7 @@ ActiveRecord::Schema.define(version: 2019_05_30_201211) do
   end
 
   create_table "discounts", force: :cascade do |t|
-    t.integer "partner_id"
+    t.bigint "partner_id"
     t.string "title"
     t.string "description"
     t.integer "number_to_be_used", default: 1
@@ -96,4 +99,8 @@ ActiveRecord::Schema.define(version: 2019_05_30_201211) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "client_discounts", "clients"
+  add_foreign_key "client_discounts", "discounts"
+  add_foreign_key "discounts", "partners"
 end
